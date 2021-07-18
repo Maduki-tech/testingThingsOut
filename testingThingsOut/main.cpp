54c14950a76e2830b7b8999424541c6dbe0a7fb3
Maduki-tech <maduki.business@gmail.com>
2021-07-18 20:47:49 +0200

    Initial Commit

---
+++ testingThingsOut/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testingThingsOut
+//
+//  Created by David Schlüter on 18.07.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
